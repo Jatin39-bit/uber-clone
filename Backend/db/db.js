5f0f53c32a3486746746671b938367927acb836f
Jatin39-bit <techrevain@gmail.com>
2025-02-18 15:11:46 +0530

    Update db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose")
 
 function connectToDb() {
-    let mongodbAtlas = "mongodb+srv://hoodajatin88:ZAQwsxzaqWSX12%23@cluster0.dtjs0.mongodb.net/Uber?retryWrites=true&w=majority&appName=Cluster0"
-    mongoose.connect(process.env.DB_CONNECT || mongodbAtlas, {
+    mongoose.connect(process.env.DB_CONNECT , {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => { console.log("connected to database") }
